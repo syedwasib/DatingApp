1adc2c44f938249946cdf20db3f3a24f9773bdb7
syedwasib <syedwasib91@gmail.com>
2019-07-27 03:33:28 -0700

    repositoy

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Data
+{
+    public class IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
