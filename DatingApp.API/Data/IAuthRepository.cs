d083702e489d88e7d06abac3a47652d2b5a1dac5
syedwasib <syedwasib91@gmail.com>
2019-07-27 23:51:00 -0700

    3rd commit

--- DatingApp.API/Data/IAuthRepository.cs
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -1,9 +1,12 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
 namespace DatingApp.API.Data
 {
-    public class IAuthRepository
+    public interface IAuthRepository
     {
-        Task<User> Register(User user, string password);
-        Task<User> Login(string username, string password);
-        Task<bool> UserExists(string username);
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
     }
 }
